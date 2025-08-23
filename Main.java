17e02cf9f29cda6896c9cbda5a3b1916dc238258
vaniwooo <83522768+vanistillhere@users.noreply.github.com>
2025-08-23 13:09:01 +0000

    super sigma ily

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // seatwork five wowwww
+        System.out.println("SEATWORK FIVE");
+
+        int countdown = 3;
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        };
+        
+        // seatwork six ang galing talaga ni Gandeza sir omg
+        System.out.println("SEATWORK SIX");
+        
+        int dice = 1;
+        while (dice < 6) {
+            if (dice < 6) {
+                System.out.println("Si Gandeza: ANG POGI NI SIR LAWRENCE!!!!");
+            } else {
+                System.out.println("A3: SUPER!!!");
+            }
+            dice = dice + 1;
+        };
+    }
+}
