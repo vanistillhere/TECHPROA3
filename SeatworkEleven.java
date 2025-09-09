3e2eacdb4035e72c60a5e48f4f7121fde34eb166
vaniwooo <83522768+vanistillhere@users.noreply.github.com>
2025-09-09 17:47:17 +0800

    ang cute mo din

---
+++ SeatworkEleven.java
@@ -0,0 +1,16 @@
+public class SeatworkEleven {
+	public static void main(String[] args) {
+		System.out.println("--- Seatwork 11 Part 1 ---");
+		int[][] myNums = {{1,4,2}, {3,6,8}};
+		myNums[1][2] = 9;
+		System.out.println(myNums[1][2]);
+		
+		System.out.println("--- Seatwork 11 Part 2 ---\n");
+		int[][] myInts = {{1,4,2},{3,6,8,5,2}};
+		for (int row = 0; row < myInts.length; row++) {
+			for (int col = 0; col < myInts[row].length; col++) {
+				System.out.println("myInts[" + row + "][" + col + "] = " + myInts[row][col]);
+			}
+		}
+	}
+}
